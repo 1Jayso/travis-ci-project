5eb940dc692117fc50a558b75a0e9aa98530796c
SJA <adodey1@yahoo.com>
2023-02-01 11:01:44 +0000

    testing env vars

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const favoriteColor = process.env.FAVORITE_COLOR;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
+    console.log(`My favorite color is ${favoriteColor}`);
     console.log("Microservices rock!");
     await sleep(5000);
   }
